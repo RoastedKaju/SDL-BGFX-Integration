8867e69565c3dcc528e81d18210f00a44983f89b
R.Haris Nauman <208430036+RoastedKaju@users.noreply.github.com>
2025-10-07 03:12:15 +0500

    Fix camera

--- src/main.cpp
+++ src/main.cpp
@@ -143,10 +143,6 @@ int main() {
       // Game loop
 
       // BGFX render call
-      bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x303030ff,
-                         1.0f, 0);
-      bgfx::setViewRect(0, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-
       bgfx::touch(0);
 
       bgfx::dbgTextClear();
